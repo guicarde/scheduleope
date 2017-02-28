07d382123be4c177c1c3c9ae5d2f4657ef4b1d12
guicarde <guicarde@pe.ibm.com>
2017-02-28 04:33:38 -0500

    si llegamos aqui
    prueba

--- DAO/Conexion.php
+++ DAO/Conexion.php
@@ -7,9 +7,12 @@ $user = "vvggesku";
 $pass = "aSmF1ip7Fb_DltIN8C9EPT-hIY9apVd9";
 $db_name ="vvggesku";
 $port = "5432";
+
+var_dump('SI LLEGAMOS AQUI');
+exit();
 function Conectar()
 {
-    $con = pg_connect("host=jumbo.db.elephantsql.com port=5432 dbname=vvggesku user=vvggesku password=aSmF1ip7Fb_DltIN8C9EPT-hIY9apVd9") or die("error");
+    $con = pg_connect("host=jumbo.db.elephantsql.com port=5432 dbname=vvggesku user=vvggesku password=aSmF1ip7Fb_DltIN8C9EPT-hIY9apVd9") or die("PROBLEMAS AL LOGRAR CONEXIÓN");
     return $con;
 }
 

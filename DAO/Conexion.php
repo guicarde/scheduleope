eb9e5034b873c0b91ccbc32cb314431fe9f95fb4
guicarde <guicarde@pe.ibm.com>
2017-02-28 03:45:35 -0500

    actualizacion conexionex
    prueba

--- DAO/Conexion.php
+++ DAO/Conexion.php
@@ -1,5 +1,12 @@
 <?php
-
+$services = getenv("VCAP_SERVICES");
+$services_json = json_decode($services, true);
+$elephantsql_config = $services_json["elephantsql"][0]["credentials"];
+$host = "jumbo.db.elephantsql.com";
+$user = "vvggesku";
+$pass = "aSmF1ip7Fb_DltIN8C9EPT-hIY9apVd9";
+$db_name ="vvggesku";
+$port = "5432";
 function Conectar()
 {
     $con = pg_connect("host=jumbo.db.elephantsql.com port=5432 dbname=vvggesku user=vvggesku password=aSmF1ip7Fb_DltIN8C9EPT-hIY9apVd9") or die("error");

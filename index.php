10030339f15286412777c7644857820b24a275e3
guicarde <guicarde@pe.ibm.com>
2017-03-08 09:13:56 -0500

    cambio en cadena conexion

--- index.php
+++ index.php
@@ -6,9 +6,9 @@
 </head>
 <body>
 <?php
-$services = getenv("VCAP_SERVICES");
+$services = getenv('VCAP_SERVICES');
 $services_json = json_decode($services, true);
-$elephantsql_config = $services_json["elephantsql"][0]["credentials"];
+$elephantsql_config = $services_json['elephantsql'][0]['credentials'];
 $host = 'jumbo.db.elephantsql.com';
 $user = 'vvggesku';
 $pass = 'aSmF1ip7Fb_DltIN8C9EPT-hIY9apVd9';

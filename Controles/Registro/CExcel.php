6799b8f8c60970d6d0e299f364aa1c875b5a06bf
guicarde <guicarde@pe.ibm.com>
2017-03-22 18:53:57 -0500

    actrualizar

--- Controles/Registro/CExcel.php
+++ Controles/Registro/CExcel.php
@@ -43,7 +43,7 @@ if (isset($_POST['hidden_excel'])) {
         
         $MaxArreglo=array();
         
-        for ($row = 13; $row <= 2290; $row++)
+        for ($row = 13; $row <= 2085; $row++)
         {
             //extraer fila
             $rowData = $sheet->rangeToArray('A' . $row . ':' . 'AE' . $row,NULL, TRUE, FALSE);

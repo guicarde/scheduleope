39c93f5e08aee847ca202dbc4360e24054a95349
guicarde <guicarde@pe.ibm.com>
2017-04-12 08:25:38 -0500

    Update Conexion.php

--- DAO/Conexion.php
+++ DAO/Conexion.php
@@ -12,8 +12,8 @@ $port = "5432";
 
 function Conectar()
 {
-    //$con = pg_connect("host=jumbo.db.elephantsql.com port=5432 dbname=vvggesku user=vvggesku password=aSmF1ip7Fb_DltIN8C9EPT-hIY9apVd9") or die("PROBLEMAS AL LOGRAR CONEXIÓN");
-    $con = pg_connect("host=9.6.98.139 port=5432 dbname=BDSCHEDULE user=postgres password=123456") or die("PROBLEMAS AL LOGRAR CONEXION");
+    $con = pg_connect("host=jumbo.db.elephantsql.com port=5432 dbname=vvggesku user=vvggesku password=aSmF1ip7Fb_DltIN8C9EPT-hIY9apVd9") or die("PROBLEMAS AL LOGRAR CONEXIÓN");
+    //$con = pg_connect("host=9.6.98.139 port=5432 dbname=BDSCHEDULE user=postgres password=123456") or die("PROBLEMAS AL LOGRAR CONEXION");
     return $con;
 }
 
